253f7c1e3f3250e825b4c8cad0a7510c64818280
ReaperSycthe <kaszas.viktor1993@gmail.com>
2024-04-07 16:34:06 +0200

    Update sql_init.sql

--- sql_init.sql
+++ sql_init.sql
@@ -3,7 +3,7 @@ CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE mi;
 
-/*Felhasználók*/
+/*FelhasznÃ¡lÃ³k*/
 CREATE TABLE IF NOT EXISTS users (
   Id int(10) unsigned NOT NULL auto_increment,
   FirstName varchar(32) NOT NULL ,
@@ -17,7 +17,7 @@ CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE UNIQUE INDEX IF NOT Exists uniqueUserName on users (UserName);
 
-/*utazások*/
+/*utazÃ¡sok*/
 CREATE TABLE IF NOT EXISTS trips (
 	TripId INT NOT NULL AUTO_INCREMENT,
     Country varchar(128),
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS trips (
 ENGINE = MYISAM
 CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-/*képek*/
+/*kÃ©pek*/
 CREATE TABLE IF NOT EXISTS images (
 	ImageId INT NOT NULL AUTO_INCREMENT,
     TripId INT NOT NULL,
@@ -52,16 +52,16 @@ select
     'Valami utca 2.',
     '<div>
         <h1>Mi a Lorem Ipsum?</h1>
-        <p>A Lorem Ipsum egy egyszer? szövegrészlete, szövegutánzata a bet?szed? és nyomdaiparnak. A Lorem Ipsum az 1500-as évek óta standard szövegrészletként szolgált az iparban; mikor egy ismeretlen nyomdász összeállította a bet?készletét és egy példa-könyvet vagy szöveget nyomott papírra, ezt használta. Nem csak 5 évszázadot élt túl, de az elektronikus bet?készleteknél is változatlanul megmaradt. Az 1960-as években népszer?sítették a Lorem Ipsum részleteket magukbafoglaló Letraset lapokkal, és legutóbb softwarekkel mint például az Aldus Pagemaker.</p><br>
+        <p>A Lorem Ipsum egy egyszer? szÃ¶vegrÃ©szlete, szÃ¶vegutÃ¡nzata a bet?szed? Ã©s nyomdaiparnak. A Lorem Ipsum az 1500-as Ã©vek Ã³ta standard szÃ¶vegrÃ©szletkÃ©nt szolgÃ¡lt az iparban; mikor egy ismeretlen nyomdÃ¡sz Ã¶sszeÃ¡llÃ­totta a bet?kÃ©szletÃ©t Ã©s egy pÃ©lda-kÃ¶nyvet vagy szÃ¶veget nyomott papÃ­rra, ezt hasznÃ¡lta. Nem csak 5 Ã©vszÃ¡zadot Ã©lt tÃºl, de az elektronikus bet?kÃ©szleteknÃ©l is vÃ¡ltozatlanul megmaradt. Az 1960-as Ã©vekben nÃ©pszer?sÃ­tettÃ©k a Lorem Ipsum rÃ©szleteket magukbafoglalÃ³ Letraset lapokkal, Ã©s legutÃ³bb softwarekkel mint pÃ©ldÃ¡ul az Aldus Pagemaker.</p><br>
         
-        <h3>Miért használjuk?</h3>
-        <p>Ez egy régóta elfogadott tény, miszerint egy olvasót zavarja az olvasható szöveg miközben a szöveg elrendezését nézi. A Lorem Ipsum használatának lényege, hogy többé-kevésbé rendezettebb bet?ket tartalmaz, ellentétben a Tartalom helye, Tartalom helye-féle megoldással. Sok desktop szerkeszt? és weboldal szerkeszt? használja a Lorem Ipsum-ot mint alapbeállítású szövegmodellt, és egy keresés a lorem ipsum-ra sok félkész weboldalt fog eredményezni.</p><br>
+        <h3>MiÃ©rt hasznÃ¡ljuk?</h3>
+        <p>Ez egy rÃ©gÃ³ta elfogadott tÃ©ny, miszerint egy olvasÃ³t zavarja az olvashatÃ³ szÃ¶veg mikÃ¶zben a szÃ¶veg elrendezÃ©sÃ©t nÃ©zi. A Lorem Ipsum hasznÃ¡latÃ¡nak lÃ©nyege, hogy tÃ¶bbÃ©-kevÃ©sbÃ© rendezettebb bet?ket tartalmaz, ellentÃ©tben a Tartalom helye, Tartalom helye-fÃ©le megoldÃ¡ssal. Sok desktop szerkeszt? Ã©s weboldal szerkeszt? hasznÃ¡lja a Lorem Ipsum-ot mint alapbeÃ¡llÃ­tÃ¡sÃº szÃ¶vegmodellt, Ã©s egy keresÃ©s a lorem ipsum-ra sok fÃ©lkÃ©sz weboldalt fog eredmÃ©nyezni.</p><br>
 
-        <h3>Honnan származik?</h3>
-        <p>A hiedelemmel ellentétben a Lorem Ipsum nem véletlenszer? szöveg. Gyökerei egy Kr. E. 45-ös latin irodalmi klasszikushoz nyúlnak. Richarrd McClintock a virginiai Hampden-Sydney egyetem professzora kikereste az ismeretlenebb latin szavak közül az egyiket (consectetur) egy Lorem Ipsum részletb?l, és a klasszikus irodalmat átkutatva vitathatatlan forrást talált. A Lorem Ipsum az 1.10.32 és 1.10.33-as de Finibus Bonoruem et Malorum részleteib?l származik (A Jó és Rossz határai - Cicero), Kr. E. 45-b?l. A könyv az etika elméletét tanulmányozza, ami nagyon népszer? volt a reneszánsz korban. A Lorem Ipsum els? sora, Lorem ipsum dolor sit amet.. a 1.10.32-es bekezdésb?l származik.</p>
+        <h3>Honnan szÃ¡rmazik?</h3>
+        <p>A hiedelemmel ellentÃ©tben a Lorem Ipsum nem vÃ©letlenszer? szÃ¶veg. GyÃ¶kerei egy Kr. E. 45-Ã¶s latin irodalmi klasszikushoz nyÃºlnak. Richarrd McClintock a virginiai Hampden-Sydney egyetem professzora kikereste az ismeretlenebb latin szavak kÃ¶zÃ¼l az egyiket (consectetur) egy Lorem Ipsum rÃ©szletb?l, Ã©s a klasszikus irodalmat Ã¡tkutatva vitathatatlan forrÃ¡st talÃ¡lt. A Lorem Ipsum az 1.10.32 Ã©s 1.10.33-as de Finibus Bonoruem et Malorum rÃ©szleteib?l szÃ¡rmazik (A JÃ³ Ã©s Rossz hatÃ¡rai - Cicero), Kr. E. 45-b?l. A kÃ¶nyv az etika elmÃ©letÃ©t tanulmÃ¡nyozza, ami nagyon nÃ©pszer? volt a reneszÃ¡nsz korban. A Lorem Ipsum els? sora, Lorem ipsum dolor sit amet.. a 1.10.32-es bekezdÃ©sb?l szÃ¡rmazik.</p>
         
-        <p>A Lorem Ipsum alaprészlete, amit az 1500-as évek óta használtak, az érdekl?d?k kedvéért lent újra megtekinthet?. Az 1.10.32 és 1.10.33-as bekezdéseket szintén eredeti formájukban reprodukálták a hozzá tartozó angol változattal az 1914-es fordításból H. Rackhamtól.</p>     
+        <p>A Lorem Ipsum alaprÃ©szlete, amit az 1500-as Ã©vek Ã³ta hasznÃ¡ltak, az Ã©rdekl?d?k kedvÃ©Ã©rt lent Ãºjra megtekinthet?. Az 1.10.32 Ã©s 1.10.33-as bekezdÃ©seket szintÃ©n eredeti formÃ¡jukban reprodukÃ¡ltÃ¡k a hozzÃ¡ tartozÃ³ angol vÃ¡ltozattal az 1914-es fordÃ­tÃ¡sbÃ³l H. RackhamtÃ³l.</p>     
      </div>',
     '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d578.3767773149482!2d33.63755956267434!3d34.91164062718759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1shu!2shu!4v1711486320709!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
 WHERE
-	not exists (select 1 from trips where Country='Ciprus' and Hotel='La C Mer')
+	not exists (select 1 from trips where Country='Ciprus' and Hotel='La C Mer');
